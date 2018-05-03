d6dde94a34d43e3ecbe14b285f8ac8bc5a7b9941
上垣正顕 <hyyrolll@gmail.com>
2018-05-03 18:37:02 +0900

    Add mdbootstrap navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/tasks">Tasks</router-link>
+      <!--Navbar-->
+      <navbar position="top" className="indigo" name="ToDo" href="/">
+        <navbar-collapse>
+          <navbar-nav>
+            <navbar-item href="/" :active="true">Home</navbar-item>
+            <navbar-item href="/about">About</navbar-item>
+            <navbar-item href="/tasks">Tasks</navbar-item>
+            <!-- Dropdown -->
+            <navbar-item-dropdown label="Dropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </navbar-item-dropdown>
+            <!-- </ul> -->
+            <!-- Links -->
+          </navbar-nav>
+            <!-- Search form -->
+          <form class="form-inline">
+            <input class="form-control mr-auto p-2" type="text" placeholder="Search" aria-label="Search">
+          </form>
+        </navbar-collapse>
+          <!-- Collapsible content -->
+      </navbar>
+
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/navbar/Navbar";
+import NavbarItem from "@/components/navbar/NavbarItem";
+import NavbarItemDropdown from "@/components/navbar/NavbarItemDropdown";
+import NavbarNav from "@/components/navbar/NavbarNav";
+import NavbarCollapse from "@/components/navbar/NavbarCollapse";
+
+export default {
+  components: {
+    Navbar,
+    NavbarItem,
+    NavbarItemDropdown,
+    NavbarNav,
+    NavbarCollapse
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -17,14 +56,11 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.navbar .dropdown-menu a:hover {
+  color: inherit !important;
+}
+.fixed-top {
+  position: sticky;
 }
 </style>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,54 +1,99 @@
 <template>
-  <div class="container">
-    <!--Navbar-->
-    <navbar position="top" className="indigo" name="Your Logo" href="#">
-      <navbar-collapse>
-        <navbar-nav>
-          <navbar-item href="#" :active="true">Home</navbar-item>
-          <navbar-item href="#">Features</navbar-item>
-          <navbar-item href="#">Pricing</navbar-item>
-          <!-- Dropdown -->
-          <navbar-item-dropdown label="Dropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </navbar-item-dropdown>
-          <!-- </ul> -->
-          <!-- Links -->
-        </navbar-nav>
-          <!-- Search form -->
-        <form class="form-inline">
-          <input class="form-control mr-auto p-2" type="text" placeholder="Search" aria-label="Search">
-        </form>
-      </navbar-collapse>
-        <!-- Collapsible content -->
-    </navbar>
-    <!--/.Navbar-->
+  <div class="tasks">
+    <h1>This is first tasks page!</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
+    <h1>This is an tasks page</h1>
   </div>
 </template>
-
-<script>
-import Navbar from "@/components/Navbar";
-import NavbarItem from "@/components/NavbarItem";
-import NavbarItemDropdown from "@/components/NavbarItemDropdown";
-import NavbarNav from "@/components/NavbarNav";
-import NavbarCollapse from "@/components/NavbarCollapse";
-
-export default {
-  name: "NavbarPage",
-  components: {
-    Navbar,
-    NavbarItem,
-    NavbarItemDropdown,
-    NavbarNav,
-    NavbarCollapse
-  }
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.navbar .dropdown-menu a:hover {
-  color: inherit !important;
-}
-</style>
